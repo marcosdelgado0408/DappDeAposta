fa2b8ab8ed1214bb7aa6c80cb9d17eb1199ee9cf
Weverson <weversonpsf@gmail.com>
2021-04-27 14:13:12 -0300

    mudanças no contrato

--- apostas.sol
+++ apostas.sol
@@ -1,45 +1,36 @@
 
 pragma  solidity  ^0.7.0;
 
-contract  owner {
+// SPDX-License-Identifier: GPL-3.0
 
-    address owner;
-
-    constructor() {
-        owner =  msg.sender;
-    }
-
-    modifier onlyOwner {
-        require(msg.sender == owner, "Somente o dono do contrato pode invocar essa funcao!");
-        _;
-    }
-}
-
-
-
-contract Bet is owner{
+contract Bet{
     
-     struct BettedHorse{
-        uint amount;
-        uint horseNumber;
-    }
+    address owner; // endereço do dono do contrato
     
-    uint[] horses; /*cada cavalo tera um numero*/ /*MUITO ARRIACAO ISSO NAO PRECISA*/
+    struct BettedHorse{
+        uint amount; // valor da aposta
+        uint horseNumber; //numero do cavalo apostado
+    }
+    uint winner; // numero do cavalo vencedor
+    uint horseQuantity; // quantidade de cavalos
+    
     mapping(address => BettedHorse) bets;
     address[] addresses;
 
-    constructor() public{
-        horses.push(1);
-        horses.push(2);
-        horses.push(3);
-        horses.push(4);
-        horses.push(5);
+    constructor(){
+        owner = msg.sender;
+        horseQuantity = 5;
     }
+    
+    modifier onlyOwner {
+		require(msg.sender == owner, "Somente o dono do contrato pode invocar essa funcao!");
+		_;
+	}
     
 
     function betOnHorse(uint horseNumber) public payable{
         
-        require(horseNumber < horses.length); // nao e possivel apostar em cavalos que nao estajam disponiveis
+        require(horseNumber < horseQuantity); // nao e possivel apostar em cavalos que nao estajam disponiveis
         
         BettedHorse memory cavaloApostado;
         
@@ -55,13 +46,16 @@ contract Bet is owner{
     }
     
     
-    function setWinner() public view returns(uint){
-        uint random = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender))) % horses.length;
-        return random;
+    function setWinner() public onlyOwner{
+        winner = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender))) % horseQuantity;
     }
     
-    function withDraw() external{
-        uint winner = setWinner();
+    
+    function viewWinner() public view returns(uint){
+        return winner;
+    }
+    
+    function withDraw() external onlyOwner{
         address addr;
         
         for(uint i=0;i<addresses.length;i++){
